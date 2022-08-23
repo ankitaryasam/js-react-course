d1841bb1ebb5c1a89e29c7c3ba0ac812dac9e9ad
ankitaryasam <ankitrajaci@gmail.com>
2022-08-23 20:02:58 +0530

    23-august

---
+++ tut6.js
@@ -0,0 +1,9 @@
+//function parameters
+
+function greetMe(age){
+    console.log("My age is " + age);
+}
+
+//invocation mean calling of function
+greetMe(19); //19 is argument here
+
---
+++ tut7.js
@@ -0,0 +1,8 @@
+function sayHello(name){
+    console.log("Hello");
+
+    return name;
+}
+
+let val  = sayHello("Rahul");
+console.log(val);
