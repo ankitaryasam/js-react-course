63f5240909bee8fbc476072afdad0a684488509a
ankitaryasam <ankitrajaci@gmail.com>
2022-08-25 21:41:17 +0530

    25-august class updated

---
+++ aug25.js
@@ -0,0 +1,12 @@
+//travel application user object
+
+const myAppUser = {
+    username : "pradyuman",
+    email : "p@gmail.com",
+    city : "hyderabad",
+    citiesYouLike : "Delhi",
+    phoneNo : "0987654321"
+}
+
+//objectName.properties
+console.log(myAppUser.city);
